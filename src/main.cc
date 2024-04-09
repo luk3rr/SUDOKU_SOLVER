7c6278c17e6c2daf3e66a351d7537841dff7250e
Lucas Araújo <araujolucas@dcc.ufmg.br>
2024-04-09 09:39:24 -0300

    first commit

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+/*
+* Filename: main.cc
+* Created on: November  5, 2023
+* Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
+*/
+
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    std::cout << ":')" << std::endl;
+    return EXIT_SUCCESS;
+}
---
+++ test/unit/main_doctest.cc
@@ -0,0 +1,8 @@
+/*
+ * Filename: main_doctest.cc
+ * Created on: June  5, 2023
+ * Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
+ */
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
